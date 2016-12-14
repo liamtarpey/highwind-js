1603e883a417da203e4fe0834b2f2551cdcd8aba
Liam Tarpey <liam.m.tarpey@gmail.com>
2016-12-14 14:26:53 +0000

    Set up basic library functionality with two simple getById + getByClass functions

---
+++ src/index.js
@@ -0,0 +1,42 @@
+(function(window) {
+    'use strict';
+
+    function Highwind() {
+
+        /**
+         * Define our main library object
+         */
+        var Lib = {};
+
+        /**
+         * Get an element by id
+         */
+        Lib.getById = function(selector) {
+            console.log('getting by id... ', selector);
+            return document.querySelector('#' + selector);
+        };
+
+        /**
+         * Get an element by class
+         */
+        Lib.getByClass = function(selector) {
+            console.log('getting by class... ', selector);
+            return document.querySelector('.' + selector);
+        };
+
+        /**
+         * Return library object
+         */
+        return Lib;
+    }
+
+    /**
+     * Create Highwind on window object
+     */
+    if(typeof(window.Highwind) === 'undefined') {
+        window.Highwind = Highwind();
+    } else {
+        console.warning('Highwind library is already defined! Can\'t load library.');
+    }
+
+})(window);
